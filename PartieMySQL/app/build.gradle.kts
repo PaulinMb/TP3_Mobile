135ea0be0b6c56afb567030791e49f14d287a54e
PaulinMb <125313649+PaulinMb@users.noreply.github.com>
2023-11-13 13:54:40 -0500

    configuration du projet + ajout splashActivity

--- PartieMySQL/app/build.gradle.kts
+++ PartieMySQL/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.partiemysql"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.partiemysql"
@@ -33,7 +33,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.android.support:support-annotations:27.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.loopj.android:android-async-http:1.4.9")
+    implementation ("com.squareup.picasso:picasso:2.5.2")
 }
--- PartieMySQL/app/src/main/java/com/example/partiemysql/MainActivity.java
+++ PartieMySQL/app/src/main/java/com/example/partiemysql/MainActivity.java
@@ -2,13 +2,71 @@ package com.example.partiemysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Drawable> drawsPays;
+    ArrayList<String> listNomsPays;
+
+    Context context;
+    Intent intent;
+    SharedPreferences sharedPreferences;
+    String SHARED_PREF_NAME ="user_pref";
+    SharedPreferences.Editor sharedPrefEditor;
+    protected String name,surname,email,password,pays;
+
+    protected boolean isLoggedIn(){
+        return sharedPreferences.getBoolean("login",false);
+    }
+    protected void logout(){
+        sharedPrefEditor.putBoolean("login",false);
+        sharedPrefEditor.apply();
+        sharedPrefEditor.commit();
+    }
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public void init() {
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        sharedPrefEditor = sharedPreferences.edit();
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void setArraylistsPays(){
+        drawsPays = new ArrayList<Drawable>();
+        listNomsPays = new ArrayList();
+
+        TypedArray tAPays = getResources().obtainTypedArray(R.array.pays);
+        for (int i =0 ;i < tAPays.length(); i++) {
+
+            char[] ch = new char[100];
+            String nomPays = tAPays.getString(i).replaceAll("_"," ");
+            nomPays.getChars(13, nomPays.length()-4, ch,0);
+
+            listNomsPays.add(String.valueOf(ch));
+            drawsPays.add(tAPays.getDrawable(i));
+        }
     }
 }
